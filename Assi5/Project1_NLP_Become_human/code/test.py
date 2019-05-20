1a040c1c2234a01b7b8099505c31d52f21f4fabf
MustAndy <andy20030901@126.com>
2019-05-20 18:44:52 +0800

    renew

--- Assi5/Project1_NLP_Become_human/code/test.py
+++ Assi5/Project1_NLP_Become_human/code/test.py
@@ -3,9 +3,12 @@ from gensim.models.word2vec import LineSentence
 import re
 import jieba
 import pandas as pd
-
+import os
 def cut(string): return ' '.join(jieba.cut(string))
+DATA_PATH = 'd:/senior/aiCourse/dataSource/'
 
-all_word2vec = Word2Vec.load('./wiki_news_cutting.pickle')
+
+MODEL_PATH = os.path.join(DATA_PATH,'w2vmodel/wiki_news_cutting.model')
+all_word2vec = Word2Vec.load(MODEL_PATH)
 print(all_word2vec.similar_by_word('说'))
 
